6e71bec0de9cfeef8df4f4dd026d256f73549aa5
mutoch <89289398+mutoch@users.noreply.github.com>
2022-05-14 11:14:36 +0900

    React-Router-Dom

---
+++ src/router/Router.jsx
@@ -0,0 +1,55 @@
+import { Switch, Route } from "react-router-dom";
+import { Home } from "../Home";
+import { Page404 } from "../Page404";
+import { page1Routes } from "./Page1Routes";
+import { page2Routes } from "./Page2Rootes";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/" render={() => <Home />} />
+      <Route
+        path="/page1"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page1Routes.map((route) => {
+              console.log(`${url}${route.path}`);
+
+              return (
+                <Route
+                  key={route.path}
+                  exact={route.exact}
+                  path={`${url}${route.path}`}
+                >
+                  {route.children}
+                </Route>
+              );
+            })}
+          </Switch>
+        )}
+      />
+
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page2Routes.map((route) => {
+              return (
+                <Route
+                  key={route.path}
+                  exact={route.exact}
+                  path={`${url}${route.path}`}
+                >
+                  {route.children}
+                </Route>
+              );
+            })}
+          </Switch>
+        )}
+      />
+      <Route path="*">
+        <Page404 />{" "}
+      </Route>
+    </Switch>
+  );
+};
